87ebd3b9ca047eb99a84897f06df28967a03d375
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2011-05-15 18:11:38 +0200

    Modified the dx_min dx_max to be magic constants -34 and 34.

--- hlcv/exercise2/dxdy_hist.m
+++ hlcv/exercise2/dxdy_hist.m
@@ -19,13 +19,13 @@ function h=dxdy_hist(img_gray, num_bins)
   % compute the first derivatives
   sigma = 6;  % I like this number!
   [imgDx, imgDy] = gaussderiv(img_gray, sigma);
-  % For each deriv, if a is min val, and b is max val, we need num_bins / (b - a)
-  dx_rng = minmax(imgDx(:)'); dx_min = dx_rng(1); dx_c = double(num_bins) / (dx_rng(2) - dx_min) + eps;
-  dy_rng = minmax(imgDy(:)'); dy_min = dy_rng(1); dy_c = double(num_bins) / (dy_rng(2) - dy_min) + eps;
+
+  d_min = -34; d_max = 34;
+  d_c = double(num_bins) / (d_max - d_min) + eps;
 
   % Prepare derivatives so they show the exact bin.
-  imgDx = floor((imgDx - dx_min) .* dx_c) + 1;
-  imgDy = floor((imgDy - dy_min) .* dy_c) + 1;
+  imgDx = floor((imgDx - d_min) .* d_c) + 1;
+  imgDy = floor((imgDy - d_min) .* d_c) + 1;
   % If teoretically imgDx,Dy are in [1, num_bins], then in practice it is never like that.
   imgDx(imgDx > num_bins) = num_bins;
   imgDx(imgDx < 1) = 1;
