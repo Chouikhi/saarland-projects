dbc56b085f4fbc24c0690867d25c55d7c71ba52c
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2011-05-15 16:00:00 +0200

    First implementation of find_best_match.

--- hlcv/exercise2/find_best_match.m
+++ hlcv/exercise2/find_best_match.m
@@ -18,18 +18,44 @@ function [best_match, D] = find_best_match(model_images, query_images, dist_type
 
   D = zeros(length(model_images), length(query_images));
 
+  model_hists = compute_histograms(read_images_from_file_list(model_images), ...
+    hist_func, hist_isgray, num_bins);
+  query_hists = compute_histograms(read_images_from_file_list(query_images), ...
+    hist_func, hist_isgray, num_bins);
+
   % compute distance matrix
-  % ...
+  for i = 1:length(model_images)
+    for j = 1:length(query_images)
+      D(i, j) = dist_func(model_hists{i}, query_hists{j});
+    end
+  end
   
   % find the best match for each query image
-  % ...
+  [a, best_match] = min(D);
+end
+
+function image_list = read_images_from_file_list(image_file_names)
+  image_list = cell(size(image_file_names));
+  for i = 1:length(image_file_names)
+    image_list{i} = imread(image_file_names{i});
+  end
+end
 
 
 function image_hist = compute_histograms(image_list, hist_func, hist_isgray, num_bins)
   
   assert(iscell(image_list));
-  image_hist = [];
+  image_hist = cell(size(image_list));
 
-  % compute hisgoram for each image and add it at the bottom of image_hist
-  % ...
+  % compute histogram for each image and add it at the bottom of image_hist
+  for i = 1:size(image_list)
+    img = image_list{i};
+    if (hist_isgray)
+      img = rgb2gray(img);
+    end
+    img = double(img);
+    hist = hist_func(img, num_bins);
+    image_hist{i} = hist;
+  end
+end
 
