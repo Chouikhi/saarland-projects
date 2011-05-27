22bb905e380e9354f04187bc89a78ce73aaa2d26
Iskren Chernev <iskren.chernev@gmail.com>
2011-05-27 16:07:24 +0200

    Added counter for number of iterations (for debuging).

--- spa/pr1/RoundRobin.hs
+++ spa/pr1/RoundRobin.hs
@@ -6,13 +6,13 @@ import Program
 import Data.List
 
 roundRobin :: (Carrier c) => (Analysis c) -> Program -> (State c) -> (State c)
-roundRobin asys prog initState = step points initState False
+roundRobin asys prog initState = step points initState False 0
   where
     points = programPoints prog
-    vars = programVars prog
-    step [] state False = state
-    step [] state True  = step points state False
-    step (p:ps) state changed = step ps state' changed'
+    step [] state False _ = state
+    -- step [] state True 1 = state
+    step [] state True iter  = step points state False (iter + 1)
+    step (p:ps) state changed iter = step ps state' changed' iter
       where
         (state', change) = eval asys prog p state
         changed' = changed || change
