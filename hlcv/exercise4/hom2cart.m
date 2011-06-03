243e730937e025956b99af3f2f33bedb251cd168
Zori Kostadinova <zornitsa.kostadinova.bg@gmail.com>
2011-06-03 04:15:35 +0200

    Fix.

--- hlcv/exercise4/hom2cart.m
+++ hlcv/exercise4/hom2cart.m
@@ -1,6 +1,11 @@
 function cart = hom2cart(hom)
   %% argument hom = 2D or 3D points in homogeneous coordinates
 
-  assert(size(cart, 1) == 3 || size(cart, 1) == 4);
-  cart = hom(1:end - 1) / hom(end);
+  assert(size(hom, 1) == 3 || size(hom, 1) == 4);
+  sz = size(hom, 2);
+  cart = [];
+  xx = hom(1:end - 1, :);
+  for i=1:sz
+    cart = [cart xx(:, i) / hom(end, i)];
+  end
 end
