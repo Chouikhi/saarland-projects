03e0f06e8b21a3c70a31c6b4aaa37100773aed50
Iskren Chernev <iskren.chernev@gmail.com>
2011-06-05 16:27:39 +0200

    Implemented Recursive fix-point algorithm.

--- spa/pr1/Main.hs
+++ spa/pr1/Main.hs
@@ -6,10 +6,12 @@ import Parser
 import Program
 import RoundRobin
 import Worklist
+import Recursive
 import qualified TrulyLiveVariables
 import qualified AvailableExpressions
 
-main = getContents >>= putStr . TrulyLiveVariables.prettyState . sort . (TrulyLiveVariables.performAnalysis roundRobin) . program . parseAnalysis . lexer
+-- main = getContents >>= putStr . TrulyLiveVariables.prettyState . sort . (TrulyLiveVariables.performAnalysis roundRobin) . program . parseAnalysis . lexer
+-- main = getContents >>= putStr . TrulyLiveVariables.prettyState . sort . (TrulyLiveVariables.performAnalysis recursive) . program . parseAnalysis . lexer
 -- main = getContents >>= putStr . TrulyLiveVariables.prettyState . sort . (TrulyLiveVariables.performAnalysis worklist) . program . parseAnalysis . lexer
--- main = getContents >>= putStr . AvailableExpressions.prettyState . sort . (AvailableExpressions.performAnalysis roundRobin) . program . parseAnalysis . lexer
+main = getContents >>= putStr . AvailableExpressions.prettyState . sort . (AvailableExpressions.performAnalysis recursive) . program . parseAnalysis . lexer
 -- main = getContents >>= putStr . AvailableExpressions.prettyState . sort . AvailableExpressions.initStateAE . program . parseAnalysis . lexer
