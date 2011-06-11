3d79b9a3a7f1eba024c457057beaa0dab0918455
Iskren Chernev <iskren.chernev@gmail.com>
2011-06-12 00:50:17 +0200

    Implemented show_false_detections.

--- hlcv/exercise5/show_false_detections.m
+++ hlcv/exercise5/show_false_detections.m
@@ -17,5 +17,27 @@
 
 function show_false_detections(figidx, pos_test_list, pos_class_score, neg_test_list, neg_class_score, num_show)
 
-% ...
+  [spcs, pidx] = sort(pos_class_score);
 
+  figure(figidx);
+  clf;
+  for i = 1:num_show
+    if spcs(i) > 0
+      % This sample is classified correctly
+      break;
+    end
+    subplot(2, num_show, i);
+    imshow(imread(pos_test_list{pidx(i)}));
+  end
+
+  [sncs, nidx] = sort(neg_class_score, 'descend');
+  for i = 1:num_show
+    if sncs(i) < 0
+      % This sample is classified correctly
+      break;
+    end
+    subplot(2, num_show, i + num_show);
+    imshow(imread(neg_test_list{nidx(i)}));
+  end
+
+end
