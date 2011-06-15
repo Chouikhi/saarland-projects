8181f1bb78e6046ad9d93ba07f3194731a7cce4b
Iskren Chernev <iskren.chernev@gmail.com>
2011-06-15 19:25:34 +0200

    Type hell (not compiling)

--- spa/pr1/FixPointAlgorithmBase.hs
+++ spa/pr1/FixPointAlgorithmBase.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE RankNTypes, PolymorphicComponents #-}
 module FixPointAlgorithmBase where
 
 import Program
 import AnalysisBase
 
-type {- (Carrier c) => -} FixPointAlgorithm c = (Analysis c) -> Program -> (State c) -> (State c)
+type {- Carrier c => -} FixPointAlgorithm c = (Analysis c) -> Program -> (State c) -> (State c)
+data AnyFixPointAlgorithm = WrapFixPointAlgorithm (forall c. Carrier c => FixPointAlgorithm c)
--- spa/pr1/Main.hs
+++ spa/pr1/Main.hs
@@ -1,15 +1,35 @@
+{-# LANGUAGE RankNTypes, GADTs #-}
+
 module Main where
 
 import Data.List (sort)
 
 import Parser
 import Program
-import RoundRobin
-import Worklist
-import Recursive
+import FixPointAlgorithmBase
+import AnalysisBase
+import Util
+import qualified RoundRobin
+import qualified Worklist
+import qualified Recursive
 import qualified TrulyLiveVariables
 import qualified AvailableExpressions
 import qualified IntervalAnalysis
+
+-- performAnalysis :: (FixPointAlgorithm CarrierAE) -> Program -> StateAE
+-- performOptimization :: Program -> StateAE -> Program
+-- transformProgram :: (Carrier c) => ((FixPointAlgorithm c) -> Program -> State c)
+--                                 -> (Program -> State c -> Program)
+--                                 -> (forall cx. Carrier cx => FixPointAlgorithm cx)
+--                                 -> Program
+--                                 -> Program
+-- transformProgram analyzer transformer fpalg prog = transformer prog (analyzer fpalg prog)
+
+-- showAnalysis :: (Carrier c, StateCls (State c)) => ((FixPointAlgorithm c) -> Program -> State c)
+--                                                 -> AnyFixPointAlgorithm --(forall cx. Carrier cx => FixPointAlgorithm cx)
+--                                                 -> Program
+--                                                 -> String
+-- showAnalysis analyzer (AFPA fpalg) prog = pretty $ analyzer fpalg prog
 
 -- main = getContents >>= putStr . TrulyLiveVariables.prettyState . sort . (TrulyLiveVariables.performAnalysis roundRobin) . program . parseAnalysis . lexer
 -- main = getContents >>= putStr . TrulyLiveVariables.prettyState . sort . (TrulyLiveVariables.performAnalysis recursive) . program . parseAnalysis . lexer
@@ -31,11 +51,50 @@ import qualified IntervalAnalysis
 --           in  putStr (prettyProgram optRes))
 
 -- XXX: Call appropriate stuff based on input.
+fixpointMap :: [(String, AnyFixPointAlgorithm)]
+fixpointMap = [ ("Round_Robin", WrapFixPointAlgorithm RoundRobin.roundRobin)
+              , ("Worklist", WrapFixPointAlgorithm Worklist.worklist)
+              , ("Recursive", WrapFixPointAlgorithm Recursive.recursive)
+              ]
+data SomeAnalizer where
+  WrapAnalizer :: forall c. Carrier c
+               => (FixPointAlgorithm c -> Program -> State c) -> SomeAnalizer
+
+data SomeOptimizer where
+  WrapOptimizer :: forall c. Carrier c
+                => (Program -> State c -> Program) -> SomeOptimizer
+
+-- analysisMap = [ ("Available_Expressions", showAnalysis
+analysisMap :: [(String, (SomeAnalizer, SomeOptimizer))]
+analysisMap = [ ( "Available_Expressions"
+                , (WrapAnalizer AvailableExpressions.performAnalysis
+                  , WrapOptimizer AvailableExpressions.performOptimization
+                  )
+                )
+              , ( "Truly_Live_Variables"
+                , ( WrapAnalizer TrulyLiveVariables.performAnalysis
+                  , WrapOptimizer TrulyLiveVariables.performOptimization
+                  )
+                )
+              , ( "Interval_Analysis"
+                , ( WrapAnalizer IntervalAnalysis.performAnalysis
+                  , WrapOptimizer IntervalAnalysis.performOptimization
+                  )
+                )
+              ]
+
 main = getContents >>= (\inp ->
-          let prog = (program . parseAnalysis . lexer) inp
-              asysRes = AvailableExpressions.performAnalysis worklist prog
-              optRes = AvailableExpressions.performOptimization prog asysRes
---           in  putStr $ show optRes)
+          let parsedInp = (parseAnalysis . lexer) inp
+              prog = program parsedInp
+              analysisName = getName $ analysis parsedInp
+              fpalgName = getName $ algorithm parsedInp
+              -- (WrapAnalizer analyzer, WrapOptimizer optimizer) = jLookup analysisName analysisMap
+              (analyzer, optimizer) = case jLookup analysisName analysisMap of
+                                        (WrapAnalizer a, WrapOptimizer o) -> (a, o)
+              (WrapFixPointAlgorithm fpalg) = jLookup fpalgName fixpointMap
+              action = getName $ output parsedInp
+              asysRes = analyzer fpalg prog
+              optRes = optimizer prog asysRes
           in  putStr (prettyProgram optRes))
 
 -- >>= putStr . TrulyLiveVariables.prettyState . sort . (TrulyLiveVariables.performAnalysis worklist) . program . parseAnalysis . lexer
