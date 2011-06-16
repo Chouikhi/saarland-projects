1736a78a53cd9be1e7cfae6e3a6c7a6f5a34eb83
Iskren Chernev <iskren.chernev@gmail.com>
2011-06-16 22:13:02 +0200

    Fixed 2 tests.

--- spa/pr1/test/lv.02.sol
+++ spa/pr1/test/lv.02.sol
@@ -1,9 +1,9 @@
 ANALYSIS_RESULTS
-    START : {R, I}
+    START : {I, R}
     P1 : {R, x}
     P2 : {R, x, y}
     P3 : {R, x, y}
-    P4 : {R, y, x}
+    P4 : {R, x, y}
     P5 : {R, x, y}
     P6 : {R, y}
     END : {}
